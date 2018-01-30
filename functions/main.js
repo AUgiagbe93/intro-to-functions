b19349c52c9c8da2137daa559035002660e28f1c
Tech-A03 <tech-a03@MacBook-Air-3.local>
2018-01-30 11:29:20 +0000

    taught basics of functions

---
+++ functions/main.js
@@ -0,0 +1,22 @@
+// Named function
+
+var result = 10 + 20;
+
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+// Annonymous function
+
+var addTwoNumbersNamed = function(){
+  return 10 + 20;
+
+
+}
+
+console.log();addTwoNumbers)
+
+//Arrow function
+
+var coolFunction() => {
+  //function code
+}
